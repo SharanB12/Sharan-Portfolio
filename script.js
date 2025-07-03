73874e2510889d2beab6ef9b668961c04d8dbafe
SharanB12 <sharan12064@gmail.com>
2025-07-03 23:09:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,9 +28,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
-  const menuToggle = document.getElementById('mobile-menu');
-  const navLinks = document.getElementById('nav-links');
-
-  menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-  });
